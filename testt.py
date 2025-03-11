662da067c75a50e18e5254c6a383bd4a6b10eb05
JGuih. <jguih@live.com>
2025-03-11 04:58:37 -0300

    Add files via upload

--- testt.py
+++ testt.py
@@ -52,66 +52,55 @@ with tab3:
     # Variável para armazenar o ticker selecionado
     ticker = None
 
-    # Seleção do ativo dentro do expander
+    # Seleção do ativo e tipo de gráfico dentro do expander
     with st.expander("Seleção de Ativo", expanded=True):
         opcao = st.radio('Selecione:', ['Índices', 'Ações', 'Commodities'])
-        with st.form(key='form_ativo'):
-            if opcao == 'Índices':
-                indices = {
-                    'IBOV': '^BVSP',
-                    'S&P500': '^GSPC',     
-                    'NASDAQ': '^IXIC',
-                    'FTSE100': '^FTSE',
-                    'DAX': '^GDAXI',
-                    'CAC40': '^FCHI',
-                    'SSE Composite': '000001.SS',
-                    'Nikkei225': '^N225',
-                    'Merval': '^MERV'
-                }
-                escolha = st.selectbox('Escolha o índice:', list(indices.keys()), index=0)
-                analisar = st.form_submit_button('Analisar')
-                if analisar:
-                    ticker = indices[escolha]
+        
+        # Seleção do tipo de gráfico dentro do expander
+        chart_type = st.selectbox("Tipo de gráfico:", ["Candlestick", "Linha"], key="chart_type")
 
-            elif opcao == 'Commodities':
-                commodities = {
-                    'Ouro': 'GC=F',
-                    'Prata': 'SI=F',
-                    'Platinum': 'PL=F',     
-                    'Cobre': 'HG=F',
-                    'WTI Oil': 'CL=F',
-                    'Brent Oil': 'BZ=F',
-                    'Gasolina': 'RB=F',
-                    'Gás Natural': 'NG=F',
-                    'Gado Vivo': 'LE=F',
-                    'Porcos Magros': 'HE=F',  # Corrigido de 'LE=F' duplicado
-                    'Milho': 'ZC=F',
-                    'Soja': 'ZS=F',
-                    'Cacau': 'CC=F',
-                    'Café': 'KC=F'
-                }    
-                escolha = st.selectbox('Escolha o commodity:', list(commodities.keys()))
-                analisar = st.form_submit_button('Analisar')
-                if analisar:
-                    ticker = commodities[escolha]
+        if opcao == 'Índices':
+            indices = {
+                'IBOV': '^BVSP',
+                'S&P500': '^GSPC',     
+                'NASDAQ': '^IXIC',
+                'FTSE100': '^FTSE',
+                'DAX': '^GDAXI',
+                'CAC40': '^FCHI',
+                'SSE Composite': '000001.SS',
+                'Nikkei225': '^N225',
+                'Merval': '^MERV'
+            }
+            escolha = st.selectbox('Escolha o índice:', list(indices.keys()), index=0)
+            ticker = indices[escolha]
 
-            elif opcao == 'Ações':
-                acoes = ['ALOS3', 'ABEV3', 'ASAI3', 'AURE3', 'AMOB3', 'AZUL4', 'AZZA3', 'B3SA3', 'BBSE3', 'BBDC3', 'BBDC4', 
-                         'BRAP4', 'BBAS3', 'BRKM5', 'BRAV3', 'BRFS3', 'BPAC11', 'CXSE3', 'CRFB3', 'CCRO3', 'CMIG4', 'COGN3', 
-                         'CPLE6', 'CSAN3', 'CPFE3', 'CMIN3', 'CVCB3', 'CYRE3', 'ELET3', 'ELET6', 'EMBR3', 'ENGI11', 'ENEV3', 
-                         'EGIE3', 'EQTL3', 'FLRY3', 'GGBR4', 'GOAU4', 'NTCO3', 'HAPV3', 'HYPE3', 'IGTI11', 'IRBR3', 'ISAE4', 
-                         'ITSA4', 'ITUB4', 'JBSS3', 'KLBN11', 'RENT3', 'LREN3', 'LWSA3', 'MGLU3', 'POMO4', 'MRFG3', 'BEEF3', 
-                         'MRVE3', 'MULT3', 'PCAR3', 'PETR3', 'PETR4', 'RECV3', 'PRIO3', 'PETZ3', 'PSSA3', 'RADL3', 'RAIZ4', 
-                         'RDOR3', 'RAIL3', 'SBSP3', 'SANB11', 'STBP3', 'SMTO3', 'CSNA3', 'SLCE3', 'SUZB3', 'TAEE11', 'VIVT3', 
-                         'TIMS3', 'TOTS3', 'UGPA3', 'USIM5', 'VALE3', 'VAMO3', 'VBBR3', 'VIVA3', 'WEGE3', 'YDUQ3']
-                acoes_dict = {acao: acao + '.SA' for acao in acoes}
-                escolha = st.selectbox('Escolha a ação:', list(acoes_dict.keys()))
-                analisar = st.form_submit_button('Analisar')
-                if analisar:
-                    ticker = acoes_dict[escolha]
+        elif opcao == 'Commodities':
+            commodities = {
+                'Ouro': 'GC=F',
+                'Prata': 'SI=F',
+                'Platinum': 'PL=F',     
+                'Cobre': 'HG=F',
+                'WTI Oil': 'CL=F',
+                'Brent Oil': 'BZ=F',
+                'Gasolina': 'RB=F',
+                'Gás Natural': 'NG=F',
+                'Gado Vivo': 'LE=F',
+                'Porcos Magros': 'HE=F',
+                'Milho': 'ZC=F',
+                'Soja': 'ZS=F',
+                'Cacau': 'CC=F',
+                'Café': 'KC=F'
+            }    
+            escolha = st.selectbox('Escolha o commodity:', list(commodities.keys()))
+            ticker = commodities[escolha]
 
-    # Seleção do tipo de gráfico (fora do expander)
-    chart_type = st.selectbox("Tipo de gráfico:", ["Candlestick", "Linha"], key="chart_type")
+        elif opcao == 'Ações':
+            acoes = ["PETR4", "VALE3", "ITUB4", "BBAS3", "BBDC4", "RAIZ4", "PRIO3", "VBBR3", "CSAN3", "UGPA3", "BPAC11", "SANB11",
+                    "GGBR4", "CSNA3", "USIM5", "JBSS3", "ABEV3", "MRFG3", "BRFS3", "BEEF3", "ELET3", "NEOE3", "CPFE3", "ENGI11",
+                    "EQTL3", "SUZB3", "KLBN11", "DTEX3", "RANI3", "MRFG3", "CYRE3", "MRVE3", "EZTC3", "CVCB3", "TRIS3", "WEGE3", "B3SA3"]
+            acoes_dict = {acao: acao + '.SA' for acao in acoes}
+            escolha = st.selectbox('Escolha a ação:', list(acoes_dict.keys()))
+            ticker = acoes_dict[escolha]
 
     # Função para obter dados da ação
     def get_stock_data(ticker, period, interval):
