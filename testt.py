541b9eb773fe84a1454f2b567b84c8e1322202fd
JGuih. <jguih@live.com>
2025-03-11 20:48:09 -0300

    Add files via upload

--- testt.py
+++ testt.py
@@ -369,7 +369,7 @@ with col2:
                     font=dict(size=12, color='#FFFFFF'),
                     bgcolor='rgba(0, 0, 0, 0.5)',  # Fundo semi-transparente para legibilidade
                     bordercolor='#FFFFFF',
-                    borderwidth=1
+                    borderwidth=,
                     xanchor="left",  # Ancorar o texto à esquerda para não invadir o gráfico
                     yanchor="middle"  # Centralizar verticalmente no preço atual
                     )
