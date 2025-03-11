9fdb1cafedd834295aff2564c10bfbf9441645ed
JGuih. <jguih@live.com>
2025-03-11 03:49:19 -0300

    Add files via upload

--- testt.py
+++ testt.py
@@ -79,7 +79,7 @@ with tab3:
                 template="plotly_dark",
                 height=700,
                 xaxis=dict(
-                    rangeslider=dict(visible=True, thickness=0.02),
+                    rangeslider=dict(visible=True, thickness=0.01),
                     rangeselector=dict(
                             buttons=list([
                                 dict(count=1, label="1m", step="month", stepmode="backward"),
