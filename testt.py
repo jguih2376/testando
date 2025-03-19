119135f8d01f812b170dc1005b653add96fc97ab
JGuih. <jguih@live.com>
2025-03-19 12:00:55 -0300

    Add files via upload

--- testt.py
+++ testt.py
@@ -7,23 +7,15 @@ st.title("Links de Apoio")
 st.markdown("Acesse o [Yahoo Finance](https://finance.yahoo.com) para realizar suas buscas.")
 st.markdown("Acesse o [Investing](https://br.investing.com/) para realizar suas buscas.")
 st.markdown("Acesse o [Fundamentus](https://www.fundamentus.com.br/index.php) para realizar suas buscas.")
-st.markdown("Acesse o [Google](https://www.google.com) para realizar suas buscas.")
-st.markdown("Acesse o [Google](https://www.google.com) para realizar suas buscas.")
+st.markdown("Acesse o [Google](https://www.infomoney.com.br) para real")
 
 
-tab1, tab2 = st.tabs(["Links", "Feedback"])
-with tab1:
-    st.header("Links de Apoio")
-    st.markdown("Acesse os links acima para informações financeiras.")
-with tab2:
-    st.header("Feedback")
-    st.text_area("Deixe seu feedback aqui:")
+
 
 
 # Link simples com markdown
 st.markdown("Acesse o [Google](https://www.google.com) para realizar suas buscas.")
 
 # Botão que redireciona para um link
-if st.button("Visitar o Streamlit"):
-    st.write("Abrindo o site do Streamlit...")
-    st.markdown("<a href='https://streamlit.io' target='_blank'>Clique aqui para abrir</a>", unsafe_allow_html=True)
+if st.button("Deixe seu Feedback"):
+    st.markdown("[Feedback](https://forms.gle/M3abZwUMnBjBUi1q6)")
